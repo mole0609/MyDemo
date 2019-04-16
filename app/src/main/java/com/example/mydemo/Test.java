08c4a7c303fd9ac8789fddcb9b444c0d5868cb7c
unknown <mole@qq.com>
2019-04-16 21:42:05 +0800

    update mvpdemo

--- app/src/main/java/com/example/mydemo/Test.java
+++ app/src/main/java/com/example/mydemo/Test.java
@@ -2,11 +2,22 @@ package com.example.mydemo;
 
 public class Test {
     public static void main(String[] args) {
+        int arr[] = {7, 2, 3, 4, 5, 1,6};
+        maopaosort(arr);
     }
-    class ABC
-    {
-        public ABC(String a,int b){
 
+    private static void maopaosort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int num : arr){
+            System.out.println(num);
         }
     }
 }
